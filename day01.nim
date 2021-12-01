ccf9c06cf6a22c12d4b65f5c597f0f6bf6b89b37
Rolee <loczaj@users.noreply.github.com>
2021-12-01 09:10:14 +0100

    Update day01.nim
    Some simplification for day 1

--- day01.nim
+++ day01.nim
@@ -1,6 +1,6 @@
 import strutils, sequtils
 
-let input = readFile("./input01.txt").splitLines.map(parseInt)
+let input = readFile("input01.txt").splitLines.map(parseInt)
 
-echo (0 ..< input.len - 1).toSeq.countIt(input[it] < input[it+1])
-echo (0 ..< input.len - 3).toSeq.countIt(input[it] < input[it+3])
+echo toSeq(0 ..< input.len - 1).countIt input[it] < input[it+1]
+echo toSeq(0 ..< input.len - 3).countIt input[it] < input[it+3]
