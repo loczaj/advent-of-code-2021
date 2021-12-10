b9052d28711e13607b5f9f2d161d8af1b6eb6bf8
Roli <loczaj@users.noreply.github.com>
2021-12-10 20:43:56 +0100

    Simplify day 7

--- day07.nim
+++ day07.nim
@@ -3,23 +3,20 @@ import strutils, sequtils, math, stats, algorithm
 let input = readFile("input07.txt").splitLines()[0].split(",").map parseInt
 
 var
-  fuel1, fuel2, fuel3: int 
+  fuel1, fuel2l, fuel2h: int 
   statistics: RunningStat
 
 statistics.push(input)
 
 let
-  pos1 = input.sorted()[input.len div 2]
-  pos2 = statistics.mean().int
-  pos3 = statistics.mean().int + 1
+  median = input.sorted()[input.len div 2]
+  meanLow = statistics.mean().int
+  meanHigh = statistics.mean().int + 1
 
 for position in input:
-  let dist1 = abs(position - pos1) 
-  let dist2 = abs(position - pos2) 
-  let dist3 = abs(position - pos3) 
-  fuel1 += dist1
-  fuel2 += (dist2 + dist2 ^ 2) div 2
-  fuel3 += (dist3 + dist3 ^ 2) div 2
+  fuel1 += abs(position - median)
+  fuel2l += (abs(position - meanLow) + (position - meanLow) ^ 2) div 2
+  fuel2h += (abs(position - meanHigh) + (position - meanHigh) ^ 2) div 2
 
 echo fuel1
-echo min(fuel2, fuel3)
+echo min(fuel2l, fuel2h)
