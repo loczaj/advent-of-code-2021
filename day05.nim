f680b1073c7c25298fd59c560f6151bd8d26ba91
Roli <loczaj@users.noreply.github.com>
2021-12-12 18:46:34 +0100

    Beautify day 5

--- day05.nim
+++ day05.nim
@@ -9,11 +9,11 @@ let ylines = input.filterIt (it[2] == it[4])
 
 for (_, x0, y0, x1, y1) in xlines:
   for y in min(y0, y1) .. max(y0, y1):
-    inc map[x0][y]
+    inc map[x0, y]
 
 for (_, x0, y0, x1, y1) in ylines:
   for x in min(x0, x1) .. max(x0, x1):
-    inc map[x][y0]
+    inc map[x, y0]
 
 echo map.count cell => cell > 1
 
@@ -23,11 +23,11 @@ let uplines = input.filterIt (it[1] < it[3] and it[2] > it[4]) or (it[1] > it[3]
 for (_, x0, y0, x1, y1) in downlines:
   let y0 = min(y0, y1)
   for x in min(x0, x1) .. max(x0, x1):
-    inc map[x][y0 + x - min(x0, x1)]
+    inc map[x, y0 + x - min(x0, x1)]
 
 for (_, x0, y0, x1, y1) in uplines:
   let y0 = max(y0, y1)
   for x in min(x0, x1) .. max(x0, x1):
-    inc map[x][y0 - x + min(x0, x1)]
+    inc map[x, y0 - x + min(x0, x1)]
 
 echo map.count cell => cell > 1
